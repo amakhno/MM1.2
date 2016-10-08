c0e6b8f7167721329f69bef7273d45f757a6be96
Artem Makhno <artem_mahno@mail.ru>
2016-10-08 10:07:10 +0300

    Рабочая версия

--- DrawD2X/Result.cs
+++ DrawD2X/Result.cs
@@ -11,12 +11,60 @@ namespace DrawD2X
         public double[] x;
         public double[] t;
         public double[,] u;
+        public double[,] uA;
 
-        public Result(double[] x, double[] t, double[,] u)
+        public double xMax;
+        public double xMin;
+
+        public double uMax;
+        public double uMin;
+
+        public Result(double[] x, double[] t, double[,] u, double[,] uA)
         {
             this.x = x;
             this.t = t;
             this.u = u;
+            this.uA = uA;
+            xMax = this.x.Max();
+            xMin = this.x.Min();
+            uMax = GetUMax();
+            uMin = GetUMin();
+        }
+
+        private double GetUMax()
+        {
+            double result = -999999999;
+
+            for(int i = 0;i<u.GetLength(0); i++)
+            {
+                for (int j = 0; j < u.GetLength(0); j++)
+                {
+                    if (result < u[i,j])
+                    {
+                        result = u[i, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private double GetUMin()
+        {
+            double result = 999999999;
+
+            for (int i = 0; i < u.GetLength(0); i++)
+            {
+                for (int j = 0; j < u.GetLength(0); j++)
+                {
+                    if (result > u[i, j])
+                    {
+                        result = u[i, j];
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
